acb09e25b9e7d22f7cb5eccb9ab01ba1e485b8fc
5yrupsandwich <60043283+omkashyap7@users.noreply.github.com>
2023-06-28 10:29:00 +0530

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,9 @@
+//activate the menubar
+// const menu = document.querySelector(".toggle");
+// const navmenu = document.querySelector(".list ");
+
+// menu.addEventListener("click", () => {
+//     menu.classList.toggle("active");
+//     navmenu.classList.toggle("active");
+// })
+ 
